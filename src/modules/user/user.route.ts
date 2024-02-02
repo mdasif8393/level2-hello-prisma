6c302e711c89350e3e2c831ae4bda066ccc7db43
unknown <md.asif.8393@gmail.com>
2024-02-02 19:38:53 +0600

    [Done--> create / update profile]

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post("/createUser", UserController.insertIntoDB);
 
+router.post("/profile", UserController.insertOrUpdateProfile);
+
 export const UserRoutes = router;
