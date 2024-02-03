baa1a4509407be160e5e88429698683dd62d201c
unknown <md.asif.8393@gmail.com>
2024-02-03 12:08:17 +0600

    [create post]

---
+++ src/modules/post/post.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { PostService } from "./post.service";
+
+const createPostController = async (req: Request, res: Response) => {
+  try {
+    const result = await PostService.createPost(req.body);
+    res.send({
+      success: true,
+      message: "Post created successfully",
+      data: result,
+    });
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+export const PostController = {
+  createPostController,
+};
---
+++ src/modules/post/post.service.ts
@@ -0,0 +1,18 @@
+import { Post, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const createPost = async (data: Post): Promise<Post> => {
+  const result = await prisma.post.create({
+    data,
+    include: {
+      author: true,
+      category: true,
+    },
+  });
+  return result;
+};
+
+export const PostService = {
+  createPost,
+};
